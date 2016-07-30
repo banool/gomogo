45f90677c1cfd89379b1f66284e1b316657dee4d
Bobby Koteski <Bobby Koteski>
2016-07-31 00:36:17 +1000

    moved server to data/... and the server can now handle 'ip/3xxx&3yyy&....' requests.

--- data/suburb-profile.py
+++ data/suburb-profile.py
@@ -7,19 +7,25 @@ import csv
 # dictionary to store csv data into program memory
 local = {}
 
+# load suburb names into local memory
+with open('suburbnames.csv','r') as csvfile:
+	reader = csv.reader(csvfile,delimiter=',')
+	for row in reader:
+		local[row[2]] = {'name' : row[1]}
+
 # read crime data
 with open('crime.csv', 'r') as csvfile:
 	reader = csv.reader(csvfile,delimiter=',')
 	for row in reader:
-		try:
-			local[row[0]] = {'crime' : int(row[5])}
-		except ValueError:
-			pass
+		suburb = row[0]
+		if suburb in local:
+			local[suburb]['crime'] = int(row[5])
 
 # read census data
 with open('census.csv', 'r') as csvfile:
 	reader = csv.reader(csvfile,delimiter=',')
 	for row in reader:
+		# parse suburb, format:POA3xxx
 		suburb = row[0][3:]
 		if suburb in local:
 			local[suburb]['medianAge'] = int(row[1])
@@ -27,6 +33,21 @@ with open('census.csv', 'r') as csvfile:
 			local[suburb]['medianSalary'] = int(row[3])
 			local[suburb]['medianRent'] = float(row[4])
 			local[suburb]['familySalary'] = int(row[5])
-			local[suburb]['avergePersons'] = float(row[7])
+			local[suburb]['averagePersons'] = float(row[7])
+
+# precess foi data
+with open('foi.csv','r') as csvfile:
+	reader = csv.reader(csvfile,delimiter=',')
+	for row in reader:
+		suburb = row[0]
+		if suburb in local:
+			local[suburb][row[2]] = 0
+	# go back to the start of the file
+	csvfile.seek(0)
+	for row in reader:
+		suburb = row[0]
+		if suburb in local:
+			local[suburb][row[2]] += 1
+			
 
 print local['3000']
