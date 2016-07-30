5c24177987232a18cf08a519c313e7aff1fe645c
Ben Rhodes <ben.rhodes@fairfaxmedia.com.au>
2016-07-30 15:56:15 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+var serverStatus = require('express-server-status');
+var express = require('express');
+var url = require('url');
+var _ = require('lodash');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.locals._ = _;
+
+var middleware = {
+
+    render: function (view) {
+        return function (req, res, cb) {
+            res.render(view);
+        }
+    },
+
+    globalLocals: function (req, res, cb) {
+        
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+        var thing = serverStatus(app);
+        res.locals = { 
+            title: 'travelocal',
+            sitebreakpoint: 'mobile',
+            description: 'A description of the site',
+            query: query,
+            param: {}
+        };
+
+        //overwrite locals based on query string
+        if ( query.sitebreakpoint != undefined ) {
+            res.locals.sitebreakpoint = query.sitebreakpoint
+        }
+
+        
+
+        cb();
+    },
+
+    index: function (req, res, cb) {
+        res.locals.index_specific_variable = 'somethingindexspecific';
+        res.locals.template = 'home';
+        cb();
+    },
+
+    article: function (req, res, cb) {
+        res.locals.article_specific_variable = 'some article specifc variables';
+        res.locals.template = 'article';
+        cb();
+    }
+
+};
+
+app.use('/status', serverStatus(app));
+
+app.use(middleware.globalLocals);
+app.get('/', middleware.index, middleware.render('template/index'));
+app.get('/home', middleware.index, middleware.render('template/index'));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
