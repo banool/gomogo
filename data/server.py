e83ab4f117e00120417c23c781faa54df7dba3f3
Bobby Koteski <Bobby Koteski>
2016-07-31 11:51:24 +1000

    simplified file responsible for reading csv data

--- data/server.py
+++ data/server.py
@@ -4,61 +4,18 @@
 
 #script imports
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-from urlparse import urlparse, parse_qs
 import json
 import csv
 import re
+import SuburbData
 
 #script constants / port 80 for server, 8080 for localhost
-PORT_NUMBER = 80
+PORT_NUMBER = 8080
 
 # dictionary to store csv data into program memory
-local = {}
+local = SuburbData.get_data()
 
-# load suburb names into local memory
-with open('suburbnames.csv','r') as csvfile:
-	reader = csv.reader(csvfile,delimiter=',')
-	for row in reader:
-		local[row[2]] = {'name' : row[1]}
-
-# read crime data
-with open('crime.csv', 'r') as csvfile:
-	reader = csv.reader(csvfile,delimiter=',')
-	for row in reader:
-		suburb = row[0]
-		if suburb in local:
-			local[suburb]['crime'] = int(row[5])
-
-# read census data
-with open('census.csv', 'r') as csvfile:
-	reader = csv.reader(csvfile,delimiter=',')
-	for row in reader:
-		# parse suburb, format:POA3xxx
-		suburb = row[0][3:]
-		if suburb in local:
-			local[suburb]['medianAge'] = int(row[1])
-			local[suburb]['medianMortgage'] = int(row[2])
-			local[suburb]['medianSalary'] = int(row[3])
-			local[suburb]['medianRent'] = float(row[4])
-			local[suburb]['familySalary'] = int(row[5])
-			local[suburb]['averagePersons'] = float(row[7])
-
-# precess foi data
-with open('foi.csv','r') as csvfile:
-	reader = csv.reader(csvfile,delimiter=',')
-	for row in reader:
-		suburb = row[0]
-		if suburb in local:
-			local[suburb][row[2]] = 0
-	# go back to the start of the file
-	csvfile.seek(0)
-	for row in reader:
-		suburb = row[0]
-		if suburb in local:
-			local[suburb][row[2]] += 1
-
-#This class will handles any incoming request from
-#the browser 
+#This class will handles any incoming request from browser
 class myHandler(BaseHTTPRequestHandler):
 
 	#print welcome messege to server
@@ -100,7 +57,7 @@ class myHandler(BaseHTTPRequestHandler):
 			for postcode in requests:
 				if postcode in local:
 					data[postcode] = local[postcode]
-			obj = json.dumps(data)
+			obj = json.dumps(data,indent=4)
 			self.standard_response()
 			self.wfile.write(obj)
 		else:
